44f46c2c5e759d81841c6ac51ebb082e2112c2a0
pprasad <pankajprasad30@gmail.com>
2026-09-15 11:26:42 +0530

    Pushing latest code

--- JavaScriptPractice/JS-Loops/Loop_Practice.js
+++ JavaScriptPractice/JS-Loops/Loop_Practice.js
@@ -22,7 +22,7 @@ for (var j=1; j<=10; j++) {
 }
 
 console.log("################################################")
-// 3. Write a condition with if condition.
+// 3. Write a loop with if condition.
 // get all the numbers which is divisible by 3 and 5 from 1 to 100.
 
 for(var i=1; i<=100; i++){
@@ -33,4 +33,172 @@ for(var i=1; i<=100; i++){
 
 console.log("################################################")
 // 4. WAP to get factorial of given number.
-////////////////////////////////// Need to finish it/////////////////
+//  5 = 5*4*3*2*1
+var x = 5
+var fact = 1
+for(var i=x; i>0; i--)
+{
+    fact *= i // fact = fact * i
+}
+console.log(fact) // 120
+// 1*5 = 5 | 5*4 = 20| 20*3 = 60 | 60*2 = 120 | 120*1 = 120
+
+// Note* : Need to practise bellow code.
+console.log("################################################")
+// 5. Nested for loop
+
+// in nested for loop, for single value outer loop
+// complete inner loop will executes
+/*
+// Outer loop
+for (initialize, condition, increment/decrement){
+    // Inner loop
+     for (initialize, condition, increment/decrement) {
+        code block
+     }
+
+}
+*/
+
+for(var i = 1; i<=5; i++) { // i = 1
+    console.log("address :i :", i)
+    for(var j=1 ; j<=3; j++) {
+        console.log("item : j:", j)
+    }
+    console.log("---------")
+}
+
+console.log("##########################################")
+// write a program to get all prime number from 1 to 100
+// prime number : the number is divisible by 1 or number itself.
+
+for(var num=2; num<=100; num++) { // 2, 3, 4, 5, 6
+    var prime=true
+    for (var i=2; i<num; i++) { // (2, 3, 4, 5)
+        if(num%i == 0) { // 6%2 ==0, 
+            prime = false
+        }
+    }
+
+    if(prime == true) {
+        console.log(num) // 2, 3, 5
+    }
+}
+
+
+
+console.log("################################")
+/*
+*
+* *
+* * * 
+* * * *
+* * * * *
+* * * * * *
+*/
+
+for(var i=1; i<=6; i++) { // i=1, 2
+    var temp = ""
+    for(var j=1; j<=i; j++) { //j=(1, 1) (1, 2)
+        //temp = temp +" "+j // * *
+        temp = temp +" * "
+    } 
+    console.log(temp)
+}
+
+console.log("#######################")
+// Reverse the star pattern.
+
+for(var i=6; i>=1; i--) { // i=1, 2
+    var temp = ""
+    for(var j=1; j<=i; j++) { //j=(1, 1) (1, 2)
+        temp = temp +" "+j // * *
+        //temp = temp +" * "
+    } 
+    console.log(temp)
+}
+
+
+console.log("#################################")
+/*
+* * * * * * i=1
+*         * i=2
+*         * i=3
+*         * i=4
+*         * i=5
+* * * * * * i=6
+*/
+
+for(var i=1; i<=6; i++) {
+    var temp =""
+    for(var j=1; j<=6; j++) {
+        if (i == 1) {
+            temp = temp + " *"
+        } else if (i==2) {
+            if(j == 1 || j == 6) {
+                temp = temp + " *"
+            } else {
+                temp = temp + "  "
+            }
+        } else if(i ==3) {
+            if(j == 1 || j == 6) {
+                temp = temp + " *"
+            } else {
+                temp = temp + "  "
+            }
+
+        }
+        else {
+            temp = temp + " -"
+
+        }   
+    }
+    console.log(temp)
+}
+
+
+
+
+
+
+
+/*
+for(var i=1; i<=6; i++) {
+    var temp =""
+    for(var j=1; j<=6; j++) {
+        if(i == 1 || i == 6) {
+             temp = temp + " *"
+        } else if (i >1 && i<6) {
+            if (j> 1 && j <6) {
+                temp = temp + "  "
+            } else{
+                 temp = temp + " *"
+            }
+        }
+        else {
+            temp = temp + " -"
+        }
+       
+    }
+    console.log(temp)
+}
+
+*/
+
+/*
+
+* * * * * * *
+      *
+      * 
+      * 
+      * 
+      *  
+    
+
+*         *
+*         *
+*         *
+*         *
+* * * * * *
+
+*/
--- JavaScriptPractice/JS-String/String_Practice.js
+++ JavaScriptPractice/JS-String/String_Practice.js
@@ -48,3 +48,124 @@ for (var chr of str3) {
     }
 }
 console.log("count of vowels :", count, temp)
+
+console.log("##############################################")
+// String uppercase and lowercase.
+var str_A = "Learning Java Script"
+console.log("Uppercase output: ", str_A.toUpperCase()) // LEARNING JAVA SCRIPT
+console.log("Lowercase output : ", str_A.toLowerCase()) // learning java script
+
+
+
+console.log("##############################################")
+// index of 
+var str_A = "Learning Java Script"
+console.log("index of n: ", str_A.indexOf('n')) // 4
+console.log("index of J: ", str_A.indexOf("J")) // 9
+
+console.log("##############################################")
+// trim method : It removes all spaces from begining and end of the string.
+
+var str_b = "  Good morning  "
+// remove both side spaces
+console.log(str_b.trim())
+// remove left side space
+console.log(str_b.trimStart())
+// remove right side spaces
+console.log(str_b.trimEnd())
+
+console.log("##############################################")
+// slice method : method help to get substring from login using indexing.
+// slice(start index, end index) : output will include start index and exclude the end index value.
+// It also accepts the negative indexing as well.
+var strC = "JavaScript Is Fun"
+console.log(strC.slice(0, 10)) // JavaScript
+//output will include start index char and exclude last index character.
+// JavaScript
+console.log(strC.slice(14, 17)) // Fun
+console.log(strC.slice(4)) // Script Is Fun
+// String bupports negative indaving also.
+console.log(strC.slice(-3)) // Fun
+
+
+// get all characters and their index position using for in loop.
+var s1 = "Programming"
+for(var a in s1) {
+    console.log(a, ":", s1[a])
+}
+
+console.log("####################################")
+// substring(start, index) : it doesn't accept the negative indexing.
+// it works same as slice method.
+
+var s2 = "JavaScript Programming"
+console.log(s2.substring(11)) // Programming
+
+console.log("###############################")
+// replace method : this method replace word1 with word2 and return the output.
+var s4 = "We Are Learning Python, Its Fun to Learn Python"
+var result = s4.replace("Python", "JavaScript")
+// It will replace on first occurrences of Python with Javascript
+console.log("Result :", result)
+// We Are Learning JavaScript, Its Fun to Learn Python
+
+// ReplaceAll method : It will replace all occurrences.
+var result2 = s4.replaceAll("Python", "TypeScript")
+console.log("result2 :", result2)
+// We Are Learning TypeScript, Its Fun to Learn TypeScript
+
+console.log(result2.replaceAll(" ", ""))
+// WeAreLearningTypeScript,ItsFuntoLearnTypeScript
+
+console.log("##############################")
+//split method : this method split string from given delimeter and return as array of
+// substrings.
+
+var strF = "India Won Cricket World Cup"
+console.log(strF.split(" "))
+// [ 'India', 'Won', 'Cricket', 'World', 'Cup' ]
+
+var strG = "India-Won-Cricket-World-Cup"
+console.log(strG.split("-"))
+// [ 'India', 'Won', 'Cricket', 'World', 'Cup' ]
+
+var strF = "India$Won$Cricket$World$Cup"
+console.log(strF.split("$"))
+// [ 'India', 'Won', 'Cricket', 'World', 'Cup' ]
+
+
+console.log("###################################")
+// string concatenation.
+var strK = "Learning JavaScript"
+var strJ = "Programming"
+var strL = "Fundamentals"
+
+//1. concatenate with plus operator
+var r1 = strK +" "+strJ+" "+strL
+console.log(r1)
+
+//2. concatenation with back tick
+var r2 = `${strK} ${strJ} ${strL}`
+console.log(r2)
+
+//3. Concatenation with concat method.
+
+var r3 = strK.concat(" ", strJ).concat(" ", strL) 
+console.log(r3)
+
+
+console.log("##############################")
+// repeat() : this method repeat the given string n number of times.
+
+var str1 = "JavaScript "
+console.log(str1.repeat(5))
+// JavaScript JavaScript JavaScript JavaScript JavaScript 
+
+console.log("##############################")
+
+// get ASCII Value
+// A - Z : 65- 90
+// a - z: 97 - 122
+console.log("A".charCodeAt(0)) // 65
+console.log("Z".charCodeAt(0)) // 90
+console.log("ABC".charCodeAt(2)) // 67
