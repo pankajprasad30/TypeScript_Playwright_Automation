ea55a29b419edb37a7b65e703fceef8022e4586a
pprasad <pankajprasad30@gmail.com>
2026-09-25 14:29:16 +0530

    code updated

---
+++ Basic_Practice/String_Methods.js
@@ -0,0 +1,121 @@
+/*
+
+Javascript strings are primitive and immutable: All string methods produce a new string without altering the original string.
+
+String length
+String charAt()
+String charCodeAt()
+String codePointAt()
+String concat()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()
+String toUpperCase()
+String toLowerCase()
+String isWellFormed()
+String toWellFormed()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+*/
+
+// String length
+let text = "PANKAJ Prasad"
+let length = text.length
+console.log(length) // 13
+
+// String charAt()
+console.log(text.charAt(4)) // A
+
+// String charCodeAt()
+console.log(text.charCodeAt(3)) // 75
+// String codePointAt()
+// String concat()
+// String at()
+// String [ ]
+// String slice()
+// String substring()
+// String substr()
+// String toUpperCase()
+// String toLowerCase()
+// String isWellFormed()
+// String toWellFormed()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
